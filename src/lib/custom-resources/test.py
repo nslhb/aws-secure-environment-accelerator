0efa4f7389baa2f1cafaf51a014ed1e6407f234e
Rohit Verma <rohit.verma@nslhub.com>
2021-03-29 06:39:01 +0530

    test

--- src/lib/custom-resources/test.py
+++ src/lib/custom-resources/test.py
@@ -1,31 +1,53 @@
 import os
 import json
-rootdir = '/Users/rverma/dev/aws-secure-environment-accelerator/src/lib/custom-resources'
 
+rootdir = '/Users/rverma/dev/aws-secure-environment-accelerator/src/lib/custom-resources'
+rush = []
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if file == 'package.json':
-            with open(os.path.join(subdir, file), "r") as jsonFile:
-                print(jsonFile)
-                dependencies,peerDependencies,devDependencies = set(),set(),set()
+            with open(os.path.join(subdir, file), "r+") as jsonFile:
+                dependencies, peerDependencies, devDependencies = {}, {}, {}
                 data = json.load(jsonFile)
-                try:
-                    dependencies = set(data["dependencies"])
-                except Exception: 
-                    pass
-                try:
-                    peerDependencies=set(data["peerDependencies"])
-                except Exception: 
-                    pass
-                try:
-                    devDependencies=set(data["devDependencies"])
-                except Exception: 
-                    pass
-                incorrect=dependencies.difference(peerDependencies)
-                moved=devDependencies.union(peerDependencies)
-                data["dependencies"]=list(incorrect)
-                data["devDependencies"]=list(moved)
+                if 'peerDependencies' in data and len(data["peerDependencies"]) > 0:
+                    peerDependencies = data["peerDependencies"]
+                else:
+                    continue
+                if 'dependencies' in data and len(data["dependencies"]) > 0:
+                    dependencies = data["dependencies"]
+                    if 'ts-node' in dependencies:
+                        dependencies["ts-node"] = "8.8.1"
+                    if '@types/jest' in dependencies:
+                        dependencies["@types/jest"] = "25.2.1"
+                else:
+                    continue
+                if 'devDependencies' in data and len(data["devDependencies"]) > 0:
+                    devDependencies = data["devDependencies"]
+                    if 'ts-node' in devDependencies:
+                        devDependencies["ts-node"] = "8.8.1"
+                    if '@types/jest' in devDependencies:
+                        devDependencies["@types/jest"] = "25.2.3"
+                cleaned = dict(set(peerDependencies.items()).difference(set(dependencies.items())))
+                moved = devDependencies | peerDependencies
+                print("peer: ", cleaned)
+                print("dev: ", moved)
+                data["peerDependencies"] = cleaned
+                # data["devDependencies"] = moved
+                jsonFile.seek(0)
+                json.dump(data, jsonFile, indent=2)
+                jsonFile.truncate()
 
-                with open(file, "w") as jsonFile:
-                    json.dump(data, jsonFile)
-
+# for subdir, dirs, files in os.walk(rootdir):
+#     for file in files:
+#         if file == 'package.json':
+#             with open(os.path.join(subdir, file), "r") as jsonFile:
+#                 data = json.load(jsonFile)
+#
+#                 temp = {}
+#                 temp["projectFolder"] = "src/lib/custom-resources/"+subdir
+#                 temp["packageName"] = data["name"]
+#                 temp["shouldPublish"] = "true"
+#                 temp["versionPolicyName"]= "locked"
+#                 rush.append(temp)
+#
+# print(rush)
